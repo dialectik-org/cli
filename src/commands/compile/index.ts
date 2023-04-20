35ae937cbb3a46b12d639fee76eb12e908521656
Benoit Rognier <benoit.rognier@edukera.com>
2023-04-20 11:18:57 +0200

    fix

--- src/commands/compile/index.ts
+++ src/commands/compile/index.ts
@@ -1,7 +1,7 @@
 import { Args, Command, Flags } from '@oclif/core'
 
 export default class Compile extends Command {
-  static description = 'Compile MD source'
+  static description = 'Compile Markdown (.md) source'
 
   static examples = [
     '$ dialecli compile ./hello/hello.md --id Hello',
@@ -10,21 +10,57 @@ export default class Compile extends Command {
   static flags = {
     id: Flags.string({char: 'i', description: 'Source identifier', required: false}),
     style: Flags.string({char: 's', description: 'CSS style source', required: false}),
+    inlinestyle: Flags.boolean({ char: 'c', description: 'Inline style (see style flag)', required: false  }),
+    inlinejs: Flags.boolean({ char: 'j', description: 'Inline js', required: false  }),
   }
 
   static args = {
-    source: Args.string({description: 'Source to compile', required: true}),
+    source: Args.string({description: 'Markdown source to compile', required: true}),
   }
 
   async run(): Promise<void> {
     const {args, flags} = await this.parse(Compile)
     this.log(`Compile ${args.source}`)
+    this.log(process.cwd())
     if (flags.id) {
       this.log(`Id: ${flags.id}`)
     }
 
     if (flags.style) {
       this.log(`Style: ${flags.style}`)
+    }
+
+    if (flags.inlinejs) {
+      this.log(`Inline style: ${flags.inlinejs}`)
+    }
+
+    if (flags.inlinestyle) {
+      if (flags.style === undefined) {
+        this.warn('Flag "inlinestyle" ignored because flag style not set')
+      } else {
+        this.log(`Inline Style: ${flags.inlinestyle}`)
+      }
+    }
+
+    try {
+      const compiler = await import('@dialectik/compiler')
+      compiler.compile(
+        {
+          id: flags.id ?? 'Dialectik Content',
+          contentDirSuffix: '',
+          targetType: 'HTML',
+          sources: [args.source],
+          styles: flags.style ? [flags.style] : [],
+          components: 'Default',
+          prismStyle: undefined,
+          externalStyle: false,
+          static: false,
+          inlineCss: flags.inlinestyle,
+          inlineImage: false,
+          inlineJs: flags.inlinejs,
+        }, process.cwd())
+    } catch (error) {
+      console.error('Error importing compiler module:', error)
     }
   }
 }
